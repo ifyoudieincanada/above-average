d6720be7f9739e224f673f980726f571a7ae86a5
kyradrake <kyradrake@Kyras-MacBook-Air.local>
2016-01-16 10:57:34 -0600

    Add A Course Page is DONE
    the table finally works!

--- above-average/AddACourseViewController.swift
+++ above-average/AddACourseViewController.swift
@@ -32,10 +32,7 @@ class AddACourseViewController: UIViewController, UITableViewDelegate {
         
         courseArray.append(c!)
         
-        counter = 0
         tempCategories = []     //clear temp array
-        
-        //print(c)
     }
     
     //ASSIGNMENT CATEGORIES STUFF
@@ -49,66 +46,90 @@ class AddACourseViewController: UIViewController, UITableViewDelegate {
         //when this button is pressed, read from the categoryName nad weight text fields and create a new assignment category
         
         let weight:Int = Int(weightTextField.text!)!
+        tempCategories += [(category:categoryNameTextField.text!, weight:weight, avg:Double(weight))]
         
-        tempCategories[counter] = (category:categoryNameTextField.text!, weight:weight, avg:Double(weight))
-        //c?.addCategory((a:categoryNameTextField.text!, b:weight, avg:Double(weight)))
-        counter++
+        print(tempCategories)
+        print(tempCategories.count)
+        
+        assignmentCategoriesTable.reloadData()
+        categoryNameTextField.text = ""
+        weightTextField.text = ""
     }
     
     //ASSIGNMENT CATEGORIES TABLE STUFF
     
     //add an array for holding the assignment categories
-    
+   
     @IBOutlet weak var assignmentCategoriesTable: UITableView!
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         return tempCategories.count       //return count of the array of assignment categories
-    
+    }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
         //how to populate cells... retrieve data from list of assignment categories
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
         
         if tempCategories.count == 0 {
             cell.textLabel?.text = "No Assignment Categories Entered"
         }
+        else if tempCategories.count > 0 {
+            cell.textLabel?.text = tempCategories[indexPath.row].category + "  " + String(tempCategories[indexPath.row].weight)        //.category] + "  " + tempCategories[indexPath.row.weight]
+        }
         else {
-            cell.textLabel?.text = tempCategories[indexPath.row.category] + "  " + tempCategories[indexPath.row.weight]
-        //cell.textLabel?.text = toDoList[indexPath.row]
+            print("Error")
         }
         return cell
     }
     
-        /*
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         
         if editingStyle == UITableViewCellEditingStyle.Delete {     //swipe to the left (delete)
-            
             tempCategories.removeAtIndex(indexPath.row)
-            
             assignmentCategoriesTable.reloadData()
         }
     }
     
     override func viewDidAppear(animated: Bool) {
-        
         assignmentCategoriesTable.reloadData()
     }
     
-    //OTHER STUFF*/
+    //OTHER STUFF
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        assignmentCategoriesTable.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ above-average/common.swift
@@ -0,0 +1,27 @@
+//
+//  common.swift
+//  above-average
+//
+//  Created by Kyra Drake on 1/16/16.
+//  Copyright © 2016 Kyra Drake. All rights reserved.
+//
+/*
+import Foundation
+
+//delegate that require the side menu to open
+@objc
+protocol SideMenuDelegate {
+    optional func open_side_menu()
+}
+
+//delegate from the side menu click
+@objc
+protocol SideMenuClick {
+    var current_background: Int { get set }
+    optional func request_background_update()
+}
+
+//delegate variables
+var sidemenuclick:SideMenuClick?
+var sidemenudelegate:SideMenuDelegate?
+*/
