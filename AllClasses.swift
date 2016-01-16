806b8c476765492957dbfad796d0a7fa486ba755
sandanzuki <kyasshiou@gmail.com>
2016-01-16 01:03:08 -0600

    lol classes (mine)

---
+++ AllClasses.swift
@@ -0,0 +1,14 @@
+// JESSICA
+class Course{
+    var name:String
+    var identifier:String
+    var assignmentCategories:[(category:String,weight:Int)]
+    var assignments:[Assignment]
+    var overallPercent:Double
+    
+    init(){
+    }
+    // @TODO add assignment category
+    // @TODO remove assignment category
+    // @TODO do the categories add up to 100%?
+}
