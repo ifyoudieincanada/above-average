6e874a104a13bfa485cd0b4d5e3172b991053cdc
kyradrake <kyradrake@Kyras-MacBook-Air.local>
2016-01-16 02:18:15 -0600

    semester screen done
    UI is done for the semester (courses list) page. The swift code
    framework for this screen is done as well.

--- CoursesListViewController.swift
+++ CoursesListViewController.swift
@@ -12,6 +12,41 @@ class CoursesListViewController: UIViewController {
     
     @IBOutlet weak var menuButton: UIBarButtonItem!
     
+    @IBOutlet weak var semesterCoursesTable: UITableView!
+    
+    //SEMESTER COURSES TABLE STUFF
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1        //return number of courses in the semester
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        //cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {     //swipee to the left (delete)
+            
+            //toDoList.removeAtIndex(indexPath.row)
+            
+            semesterCoursesTable.reloadData()
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        semesterCoursesTable.reloadData()
+    }
+    
+    //OTHER STUFF
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,6 +62,4 @@ class CoursesListViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
