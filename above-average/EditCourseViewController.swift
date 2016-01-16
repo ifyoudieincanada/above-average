54a3ae8513b71b0761ec0459a851e803cd97478f
kyradrake <kyradrake@Kyras-MacBook-Air.local>
2016-01-16 04:49:09 -0600

    framework pretty much done
    yay

--- above-average/EditCourseViewController.swift
+++ above-average/EditCourseViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class EditCourseViewController: UIViewController {
     
+    @IBOutlet weak var courseTitleTextField: UITextField!
+    
+    @IBOutlet weak var courseIdentifierTextField: UITextField!
+    
+    @IBAction func saveButton(sender: AnyObject) {
+        
+        //save course data
+    }
+    
+    //ASSIGNMENT CATEGORIES TABLE STUFF
+    
+    @IBOutlet weak var assignmentCategoriesTable: UITableView!
+    
+    @IBOutlet weak var categoryTexttField: UITextField!
+    
+    @IBOutlet weak var weightTextField: UITextField!
+    
+    @IBAction func addButton(sender: AnyObject) {
+        
+        //add an assignment category
+    }
+    
+    //OTHER STUFF
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
