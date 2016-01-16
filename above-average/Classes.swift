1c5cdfcba1b0f38838e6b5f5a68eb8b1335eb90a
Nick <nicholascgilpin@yahoo.com>
2016-01-16 09:12:53 -0600

    Deleted custom date class
    It wasn't being used because we replaced it with NSDate

--- above-average/Classes.swift
+++ above-average/Classes.swift
@@ -16,7 +16,7 @@ class Course: NSObject {
     var assignmentCategories:[(category:String,weight:Int,avg:Double)]
     var assignments:[Assignment]
     var overallPercent:Double
-    
+
     init(a:String, b:String) {
         name = a
         identifier = b
@@ -24,35 +24,35 @@ class Course: NSObject {
         assignments = []
         overallPercent = 0.0
     }
-    
+
     // add assignment category
     func addCategory(cat:(a:String, b:Int, avg:Double)) {
         // @TODO check if category with that name already exists with for each
         assignmentCategories += [cat] //or use append?
     }
-    
+
     // @TODO add assignment categories on course creation page
     func addOnCreate(cats:[(a:String, b:Int, avg:Double)]) {
         assignmentCategories += cats //or use append?
     }
-    
+
     // @TODO update assignment categories
     func updateCategories() {
-        
+
     }
-    
+
     // @TODO remove assignment category
     func removeCategory(removeMe:String) {
         assignmentCategories = assignmentCategories.filter({ $0.category != removeMe })
     }
-    
+
     // @TODO add assignment
     func addAssignment(a:String, b:String, c:Bool, d:NSDate, e:Double, f:Double) {
         //create assignment
         var temp = Assignment(a: a, b: b, c: c, d: d, e: e, f: f)
         assignments += [temp] //or use append?
     }
-    
+
     // do the categories add up to 100%
     func weightCheck(assignmentCategories:[(category:String,weight:Int)]) -> Bool {
         var weightTotal:Int = 0
@@ -79,7 +79,7 @@ class Grade: NSObject {
         }
         return Double(categorySum/Double(counter))
     }
-    
+
     func calculateWeightedTotal(assignmentCategories:[(category:String,weight:Int,avg:Double)]) -> Double {
         var overall:Double = 0
         for a in assignmentCategories {
@@ -97,7 +97,7 @@ class Assignment {
     var pointsEarned:Double
     var pointsPossible:Double
     var percentage:Double
-    
+
     init(a:String, b:String, c:Bool, d:NSDate, e:Double, f:Double) {
         name = a
         category = b
@@ -107,12 +107,6 @@ class Assignment {
         pointsPossible = f
         percentage = e/f
     }
-    
-    
-}
 
-class Date {
-    init() {
-        
-    }
+
 }
