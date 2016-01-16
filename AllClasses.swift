ca926b16d9edefd9317f3c448fdc3fa8cff84375
Nick <nicholascgilpin@yahoo.com>
2016-01-16 01:08:25 -0600

    Added assignment class

--- AllClasses.swift
+++ AllClasses.swift
@@ -5,10 +5,25 @@ class Course{
     var assignmentCategories:[(category:String,weight:Int)]
     var assignments:[Assignment]
     var overallPercent:Double
-    
+
     init(){
     }
     // @TODO add assignment category
     // @TODO remove assignment category
     // @TODO do the categories add up to 100%?
+}
+
+// Nick
+class Assignment{
+    var assignmentName:String
+    var done:Bool
+    var assignmentDate:Date
+    var dueDate:Date
+    var pointsPossible:Double
+    var pointsEarned:Double
+    var finalGrade:Double
+    init() {
+      //@TODO: Figure out how to take in variables
+    }
+
 }
