ffcde68a7078f0e4147077f9dd24924469a01e72
kyradrake <kyradrake@Kyras-MacBook-Air.local>
2016-01-16 23:11:07 -0600

    Started Menu View Controller (Screen)

--- above-average/Menu.swift
+++ above-average/Menu.swift
@@ -1,5 +1,5 @@
 //
-//  AddASemesterViewController.swift
+//  Menu.swift
 //  above-average
 //
 //  Created by Kyra Drake on 1/16/16.
@@ -8,22 +8,22 @@
 
 import UIKit
 
-class AddASemesterViewController: UIViewController {
+var semesterArray = [Semester]()
 
-    @IBAction func addSemesterButton(sender: AnyObject) {
-        let s = Semester(a: semesterNameTextField.text!)
-        semesterArray.append(s)
-    }
-    
-    @IBOutlet weak var semesterNameTextField: UITextField!
-    
+class Menu: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if (NSUserDefaults.standardUserDefaults().objectForKey("semesterArray") != nil) {
+            semesterArray = NSUserDefaults.standardUserDefaults().objectForKey("semesterArray") as! [Semester]
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 }
